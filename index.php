60190b5d0f2de262c974badcb229b7fe3c4e4974
Asad Mohamed <libaaxkatib@gmail.com>
2026-07-06 22:25:40 +0300

    index and css

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 require_once "database.php";
-echo "Connected to the database!";
+
+$stmt = $db->query("SELECT * FROM products ORDER BY created_at DESC");
+$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$pageTitle = "All products";
+require_once "header.php";
+?>
